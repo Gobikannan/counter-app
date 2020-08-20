ede64e43bf1f08e8211db9a25e50c87ec3ebb627
Gobikannan Shanmugam <sgobikannan@hotmail.com>
2020-08-20 16:36:55 +0100

    render tags

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -6,43 +6,19 @@ class Counter extends Component {
     tags: ["tag1", "tag2", "tag3"],
   };
 
-  formatCount() {
-    const { count } = this.state;
-    return count === 0 ? "Zero" : count;
-  }
-
-  incrementCount() {
-    this.setState({ count: this.state.count + 1 });
-  }
-
-  getBadgeClasses() {
-    let classes = "badge m-2 badge-";
-    classes += this.state.count === 0 ? "warning" : "primary";
-    return classes;
+  renderTags() {
+    if (this.state.tags.length === 0) return <p>There are no tags!</p>;
+    return (
+      <ul>
+        {this.state.tags.map((tag) => (
+          <li key={tag}>{tag}</li>
+        ))}
+      </ul>
+    );
   }
 
   render() {
-    return (
-      <>
-        <span
-          className={this.getBadgeClasses()}
-          style={{ fontSize: 15, fontWeight: "bold" }}
-        >
-          {this.formatCount()}
-        </span>
-        <button
-          className="btn btn-secondary btn-sm"
-          onClick={() => this.incrementCount()}
-        >
-          Increment
-        </button>
-        <ul>
-          {this.state.tags.map((tag) => (
-            <li key={tag}>{tag}</li>
-          ))}
-        </ul>
-      </>
-    );
+    return <div>{this.renderTags()}</div>;
   }
 }
 
